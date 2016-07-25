b8e4c720a4ba79b26405aeb08792fbabad290d49
javaprophet <max.bruce12@gmail.com>
2016-07-24 18:19:34 -0700

    Fix incompatibilty issue with queue.

--- AvunaHTTPD/src/oqueue.c
+++ AvunaHTTPD/src/oqueue.c
@@ -12,12 +12,12 @@
 #include <pthread.h>
 #include "xstring.h"
 #include <stdlib.h>
-#include "smem.h"
+#include "util.h"
 
 struct queue* new_queue(size_t capacity, int mt) {
-	struct queue* queue = smalloc(sizeof(struct queue));
+	struct queue* queue = xmalloc(sizeof(struct queue));
 	queue->capacity = capacity;
-	queue->data = smalloc((capacity == 0 ? 1 : capacity) * sizeof(void*));
+	queue->data = xmalloc((capacity == 0 ? 1 : capacity) * sizeof(void*));
 	queue->rc = capacity == 0 ? 1 : 0;
 	queue->start = 0;
 	queue->end = 0;
@@ -25,24 +25,24 @@ struct queue* new_queue(size_t capacity, int mt) {
 	queue->mt = mt;
 	if (mt) {
 		if (pthread_mutex_init(&queue->data_mutex, NULL)) {
-			free(queue->data);
+			xfree(queue->data);
 			queue->data = NULL;
-			free(queue);
+			xfree(queue);
 			return NULL;
 		}
 		if (pthread_cond_init(&queue->out_cond, NULL)) {
-			free(queue->data);
+			xfree(queue->data);
 			queue->data = NULL;
 			pthread_mutex_destroy(&queue->data_mutex);
-			free(queue);
+			xfree(queue);
 			return NULL;
 		}
 		if (pthread_cond_init(&queue->in_cond, NULL)) {
-			free(queue->data);
+			xfree(queue->data);
 			queue->data = NULL;
 			pthread_mutex_destroy(&queue->data_mutex);
 			pthread_cond_destroy(&queue->out_cond);
-			free(queue);
+			xfree(queue);
 			return NULL;
 		}
 	}
@@ -56,9 +56,9 @@ int del_queue(struct queue* queue) {
 		if (pthread_cond_destroy(&queue->out_cond)) return -1;
 		if (pthread_cond_destroy(&queue->in_cond)) return -1;
 	}
-	free(queue->data);
+	xfree(queue->data);
 	queue->data = NULL;
-	free(queue);
+	xfree(queue);
 	return 0;
 }
 
@@ -67,14 +67,14 @@ int add_queue(struct queue* queue, void* data) {
 	if (queue->size == queue->rc && queue->capacity == 0) {
 		size_t orc = queue->rc;
 		queue->rc += 1024 / sizeof(void*);
-		void** ndata = smalloc(queue->rc * sizeof(void*));
+		void** ndata = xmalloc(queue->rc * sizeof(void*));
 		if (queue->start < queue->end) {
 			memcpy(ndata, queue->data + queue->start, (queue->end - queue->start) * sizeof(void*));
 		} else {
 			memcpy(ndata, queue->data + queue->start, (orc - queue->start) * sizeof(void*));
 			memcpy(ndata + (orc - queue->start), queue->data + queue->end, (queue->start - queue->end) * sizeof(void*));
 		}
-		free(queue->data);
+		xfree(queue->data);
 		queue->data = ndata;
 	} else if (queue->capacity == 0) {
 	} else {
@@ -89,14 +89,14 @@ int add_queue(struct queue* queue, void* data) {
 		if (queue->end - rp == queue->start) {
 			size_t orc = queue->rc;
 			queue->rc += 1024 / sizeof(void*);
-			void** ndata = smalloc(queue->rc * sizeof(void*));
+			void** ndata = xmalloc(queue->rc * sizeof(void*));
 			if (queue->start < queue->end) {
 				memcpy(ndata, queue->data + queue->start, (queue->end - queue->start) * sizeof(void*));
 			} else {
 				memcpy(ndata, queue->data + queue->start, (orc - queue->start) * sizeof(void*));
 				memcpy(ndata + (orc - queue->start), queue->data + queue->end, (queue->start - queue->end) * sizeof(void*));
 			}
-			free(queue->data);
+			xfree(queue->data);
 			queue->data = ndata;
 		} else queue->end -= rp;
 	}
