7d4ad9ad978e30dd3cf4ec91e2ec9940d962855b
JavaProphet <max.bruce12@gmail.com>
2016-08-15 23:48:02 -0700

    More buildfiles

---
+++ AvunaHTTPD/Debug/src/mime.d
@@ -0,0 +1,12 @@
+src/mime.d: ../src/mime.c ../src/mime.h ../src/xstring.h ../src/streams.h \
+ ../src/util.h ../src/globals.h
+
+../src/mime.h:
+
+../src/xstring.h:
+
+../src/streams.h:
+
+../src/util.h:
+
+../src/globals.h:
---
+++ AvunaHTTPD/Debug/src/tls.d
@@ -0,0 +1,7 @@
+src/tls.d: ../src/tls.c ../src/tls.h ../src/util.h ../src/globals.h
+
+../src/tls.h:
+
+../src/util.h:
+
+../src/globals.h:
